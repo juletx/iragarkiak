2521a2743e947cfbd52cfb853bcf23275da06e5c
Markel Azpeitia <azpeitiamarkel@gmail.com>
2019-11-27 12:13:47 +0100

    CSS hobetua

--- index.php
+++ index.php
@@ -16,12 +16,17 @@
       <div class="home">
 	    <span class="home-title"><h1>Zerren bila zabiltza?</h1></span>
 		<div class="home-kategoriak"> <ul class="home-kategoriak-lista"> <li>KOTXIAK</li> <li>MOBILAK</li> <li>ETXIAK</li></ul></div>
-		<div class="home-anuntzio">
+		<div class="shadow">
+          <div class="home-anuntzio">
 		  <div class="home-anuntzio-head"> </div>
 		  <div class="home-anuntzio-detail"> 
 		    <div class="home-anuntzio-detail-textu"> 
-			  <a href=""> VENDO OPEL CORSA NUEVO!!!!!!!!!!!!</a><br>
-			  <i class="fa fa-map-marker" style="font-size:24px">Zarautz</i>
+			  <a href=""><b>VENDO OPEL CORSA NUEVO!!!!!!!!!!!!</b></a><br>
+			  <i class="fa fa-list-alt" aria-hidden="true">  Automobilak</i>
+			  <div class="home-anuntzio-detail-textu-tokiordu">
+			  	<span class="home-anuntzio-detail-textu-toki"><i class="fa fa-map-marker" style="font-size:24px">Zarautz</i></span>
+			  	<span class="home-anuntzio-detail-textu-denbora"><i class="fa fa-clock-o" aria-hidden="true" style="font-size:24px">1 ordu</i></span>
+			  </div>
 			  <div class="home-anuntzio-detail-deskripzio">
 				Vendo renault megane motor diésel 1. 9dci 130cv con cambio manual de 6 velocidades de mediados del 2009 de los últimos de esa carrocería con 160. 000kms reales todas las revisiones pasadas en concesionario renault un solo propietario con cierre con mando eleva lunas eléctricos dirección asistida espejos eléctricos plegables climatizador ordenador de abordo radio cd con mandos al volante control de velocidad de crucero frenos abs con control de tracción llantas de aleación sensor de luces y lluviaapoyabrazos central asientos de cuero con anclaje isófix etc. . . coche en muy buen estado general motor potente y económico libro de mantenimiento en concesionario renault se aceptaría prueba mecánica en taller oficial o de confianza el coche se encuentra en almoradí se puede ver y probar. Color GRIS
 			  </div>
@@ -32,75 +37,13 @@
 		  </div>
 		  <p class="prezioa">10.000€</p><br><br><br>
 		  <div class="home-anuntzio-footer"> 
-		    <a href="#" class="home-anuntzio-footer-kontaktua">&#9742;Telefonoa</a>
+		    <a href="#" class="home-anuntzio-footer-kontaktua">Telefonoa</a>
 			<a href="#" class="home-anuntzio-footer-kontaktua">&#9993;Posta elektronikoa</a>
 		  </div>
 		</div>
-		<br>
-		<div class="home-anuntzio">
-		  <div class="home-anuntzio-head"> </div>
-		  <div class="home-anuntzio-detail"> 
-		    <div class="home-anuntzio-detail-textu"> 
-			  <a href=""> VENDO OPEL CORSA NUEVO!!!!!!!!!!!!</a><br>
-			  <i class="fa fa-map-marker" style="font-size:24px">Zarautz</i>
-			  <div class="home-anuntzio-detail-deskripzio">
-				Vendo renault megane motor diésel 1. 9dci 130cv con cambio manual de 6 velocidades de mediados del 2009 de los últimos de esa carrocería con 160. 000kms reales todas las revisiones pasadas en concesionario renault un solo propietario con cierre con mando eleva lunas eléctricos dirección asistida espejos eléctricos plegables climatizador ordenador de abordo radio cd con mandos al volante control de velocidad de crucero frenos abs con control de tracción llantas de aleación sensor de luces y lluviaapoyabrazos central asientos de cuero con anclaje isófix etc. . . coche en muy buen estado general motor potente y económico libro de mantenimiento en concesionario renault se aceptaría prueba mecánica en taller oficial o de confianza el coche se encuentra en almoradí se puede ver y probar. Color GRIS
-			  </div>
-			</div>
-			<div class="home-anuntzio-detail-irudi">
-			  <img class="home-anuntzio-detail-irudi-txiki"src="images/destrozauta.jpg">
-			</div>
-		  </div>
-		  <p class="prezioa">10.000€</p><br><br><br>
-		  <div class="home-anuntzio-footer"> 
-		    <a href="#" class="home-anuntzio-footer-kontaktua">&#9742;Telefonoa</a>
-			<a href="#" class="home-anuntzio-footer-kontaktua">&#9993;Posta elektronikoa</a>
-		  </div>
 		</div>
+		
 		<br>
-		<div class="home-anuntzio">
-		  <div class="home-anuntzio-head"> </div>
-		  <div class="home-anuntzio-detail"> 
-		    <div class="home-anuntzio-detail-textu"> 
-			  <a href=""> VENDO OPEL CORSA NUEVO!!!!!!!!!!!!</a><br>
-			  <i class="fa fa-map-marker" style="font-size:24px">Zarautz</i>
-			  <div class="home-anuntzio-detail-deskripzio">
-				Vendo renault megane motor diésel 1. 9dci 130cv con cambio manual de 6 velocidades de mediados del 2009 de los últimos de esa carrocería con 160. 000kms reales todas las revisiones pasadas en concesionario renault un solo propietario con cierre con mando eleva lunas eléctricos dirección asistida espejos eléctricos plegables climatizador ordenador de abordo radio cd con mandos al volante control de velocidad de crucero frenos abs con control de tracción llantas de aleación sensor de luces y lluviaapoyabrazos central asientos de cuero con anclaje isófix etc. . . coche en muy buen estado general motor potente y económico libro de mantenimiento en concesionario renault se aceptaría prueba mecánica en taller oficial o de confianza el coche se encuentra en almoradí se puede ver y probar. Color GRIS
-			  </div>
-			</div>
-			<div class="home-anuntzio-detail-irudi">
-			  <img class="home-anuntzio-detail-irudi-txiki"src="images/destrozauta.jpg">
-			</div>
-		  </div>
-		  <p class="prezioa">10.000€</p><br><br><br>
-		  <div class="home-anuntzio-footer"> 
-		    <a href="#" class="home-anuntzio-footer-kontaktua">&#9742;Telefonoa</a>
-			<a href="#" class="home-anuntzio-footer-kontaktua">&#9993;Posta elektronikoa</a>
-		  </div>
-		</div>
-		<br>
-		<div class="home-anuntzio">
-		  <div class="home-anuntzio-head"> </div>
-		  <div class="home-anuntzio-detail"> 
-		    <div class="home-anuntzio-detail-textu"> 
-			  <a href=""> VENDO OPEL CORSA NUEVO!!!!!!!!!!!!</a><br>
-			  <i class="fa fa-map-marker" style="font-size:24px">Zarautz</i>
-			  <div class="home-anuntzio-detail-deskripzio">
-				Vendo renault megane motor diésel 1. 9dci 130cv con cambio manual de 6 velocidades de mediados del 2009 de los últimos de esa carrocería con 160. 000kms reales todas las revisiones pasadas en concesionario renault un solo propietario con cierre con mando eleva lunas eléctricos dirección asistida espejos eléctricos plegables climatizador ordenador de abordo radio cd con mandos al volante control de velocidad de crucero frenos abs con control de tracción llantas de aleación sensor de luces y lluviaapoyabrazos central asientos de cuero con anclaje isófix etc. . . coche en muy buen estado general motor potente y económico libro de mantenimiento en concesionario renault se aceptaría prueba mecánica en taller oficial o de confianza el coche se encuentra en almoradí se puede ver y probar. Color GRIS
-			  </div>
-			</div>
-			<div class="home-anuntzio-detail-irudi">
-			  <img class="home-anuntzio-detail-irudi-txiki"src="images/destrozauta.jpg">
-			</div>
-		  </div>
-		  <p class="prezioa">10.000€</p><br><br><br>
-		  <div class="home-anuntzio-footer"> 
-		    <a href="#" class="home-anuntzio-footer-kontaktua">&#9742;Telefonoa</a>
-			<a href="#" class="home-anuntzio-footer-kontaktua">&#9993;Posta elektronikoa</a>
-		  </div>
-		</div>
-		<br>
-
 	  </div>
 	</div>
   </body>
