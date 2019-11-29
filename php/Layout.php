9f70b790b05263c1e958098cb4873b01d4d46f1e
Aitor Zubillaga <zubillagaaitor@gmail.com>
2019-11-29 17:16:21 +0100

    Formularioa hasi, oso zatarra

--- php/Layout.php
+++ php/Layout.php
@@ -60,7 +60,6 @@
 					</div>
 				</div>
 			</div>
-
 			<br>
 		</div>
 	</div>
--- php/QuestionFormWithImageHtml5.php
+++ php/QuestionFormWithImageHtml5.php
@@ -1,60 +1,119 @@
+<?php include '../php/SecurityLoggedOut.php' ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <?php include '../html/Head.html'?>
-    <script src="../js/ShowImageInForm.js"></script>
+	<?php include '../html/Head.html'?>
+	<script src="../js/VerifyPassAjax.js"></script>
 </head>
 
 <body>
-    <?php include '../php/Menus.php' ?>
-    <section class="main" id="s1">
-        <div id="form">
-            <form id="galderenF" name="galderenF"
-                action="<?php echo 'AddQuestionWithImage.php?eposta='.$_GET['eposta']?>" method="post"
-                enctype="multipart/form-data">
-                <label for="eposta">Ehuko eposta(*):</label>
-                <input type="email" id="eposta" name="eposta"
-                    pattern="([a-z]{3,}[0-9]{3}@ikasle\.ehu\.eu?s)|([a-z]+\.?[a-z]{2,}@ehu\.eu?s)" required
-                    value="<?php echo $_GET['eposta']?>" readonly>
-                <br><br>
-                <label for="galdera">Galdera(*):</label>
-                <input type="text" id="galdera" name="galdera" minlength="10" required>
-                <br><br>
-                <label for="erantzuna">Erantzun zuzena(*):</label>
-                <input type="text" id="erantzuna" name="erantzuna" required>
-                <br><br>
-                <label for="okerra1">Erantzun okerra 1(*):</label>
-                <input type="text" id="okerra1" name="okerra1" required>
-                <br><br>
-                <label for="okerra2">Erantzun okerra 2(*):</label>
-                <input type="text" id="okerra2" name="okerra2" required>
-                <br><br>
-                <label for="okerra3">Erantzun okerra 3(*):</label>
-                <input type="text" id="okerra3" name="okerra3" required>
-                <br><br>
-                <label>Zailtasuna(*):
-                    <input type="radio" id="txikia" name="zailtasuna" value="1">
-                    <label for="txikia">Txikia</label>
-                    <input type="radio" id="ertaina" name="zailtasuna" value="2" checked>
-                    <label for="ertaina">Ertaina</label>
-                    <input type="radio" id="handia" name="zailtasuna" value="3">
-                    <label for="handia">Handia</label></label>
-                <br><br>
-                <label for="gaia">Gaia(*):</label>
-                <input type="text" id="gaia" name="gaia" required>
-                <br><br>
-                <label for="argazki">Argazkia:</label>
-                <img id="argazki" alt="Aukeratu galderarekin zerikusia duen argazkia" height="100" src="#" />
-                <br><br>
-                <input type="file" id="argazkia" name="argazkia" accept="image/*" onchange="showImage(this)">
-                <br><br>
-                <input type="submit" value="Galdera gehitu">
-                <input type="reset" value="Berrezarri" onclick="hideImage()">
-            </form>
-        </div>
-    </section>
-    <?php include '../html/Footer.html' ?>
+	<?php include '../php/Menus.php' ?>
+	<section class="main" id="s1">
+		<div id="form">
+			<form id="erregistratu" name="erregistratu" action="#" method="post">
+				<fieldset>
+					<legend>
+						<h2>Anuntzioa gehitu</h2>
+					</legend>
+					<label for="category">Kategoria:</label>
+					<select name="category">
+                        <option value="automobilak">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+					<br><br>
+                    <label for="title">Titulua:</label>
+					<input type="text" id="title" name="title" required>
+					<br><br>
+					<label for="text">Textua:</label>
+					<textarea name="Text1" cols="40" rows="5" required></textarea>
+					<br><br>
+                    <label for="price">Prezioa:</label>
+					<input type="text" id="text" name="text" required>
+					<br><br>
+					<label for="city">Hiria:</label>
+					<input type="text" id="city" name="city" required>
+					<br><br>
+					<label for="city">Select Images:</label>
+					Select images: 
+                    <input type="file" name="img" multiple>
+                    <br><br>
+                    <input type="submit" id="submit" value="Sortu">
+					<input type="reset" value="Berrezarri">
+				</fieldset>
+			</form>
+
+			<?php
+			if (isset($_POST["email"])) {
+				$email = trim($_POST["email"]);
+				$password1 = trim($_POST["password1"]);
+				$password2 = trim($_POST["password2"]);
+				$name = trim($_POST["name"]);
+				$surname1 = trim($_POST["surname1"]);
+				$surname2 = trim($_POST["surname2"]);
+				$telephone = trim($_POST["telephone"]);
+
+				if (empty($email) || empty($password1) || empty($password2) || empty($name) || empty($surname1) || empty($surname2) || empty($telephone)) {
+					echo "<script>alert('Bete eremu guztiak'); history.go(-1);</script>";
+				}
+				else if (!preg_match('/[^@\s]+@[^@\s]+\.[^@\s]+/', $email)) {
+					echo "<script>alert('Eposta ez da zuzena'); history.go(-1);</script>";
+				}
+				else if (strlen($password1) < 6 || $password1 != $password2) {
+					echo "<script>alert('Pasahitzek ez dute 6ko luzera edo ez dira berdinak'); history.go(-1);</script>";
+				}
+				else if (!preg_match('/[0-9]{9}/', $telephone)) {
+					echo "<script>alert('Telefonoak 9 digitu izan behar ditu'); history.go(-1);</script>";
+				}
+				else {
+					include '../php/DbConfig.php';
+					$esteka = mysqli_connect($zerbitzaria, $erabiltzailea, $gakoa, $db) or die("Errorea datu-baseko konexioan");
+					
+					$sql = "SELECT * FROM users WHERE email='$email'";
+					$emaitza = mysqli_query($esteka, $sql);
+
+					if (!$emaitza) {
+						echo "<script>alert('Errorea datu basearen kontsultan'); history.go(-1);</script>";
+						die();
+					} else if (mysqli_num_rows($emaitza) > 0) {
+						echo "<script>alert('Eposta horrekin jadanik erabiltzaile bat dago'); history.go(-1);</script>";
+						die();
+					}
+					
+					mysqli_free_result($emaitza);
+					
+					$password_hash = password_hash($password1, PASSWORD_DEFAULT);
+
+					$sql = "INSERT INTO users VALUES ('$email', '$password_hash', '$name', '$surname1', '$surname2', '$telephone', 0)";
+					$emaitza = mysqli_query($esteka, $sql);
+		
+					mysqli_close($esteka);
+			
+					if (!$emaitza) {
+						echo "<script>alert('Erabiltzailea ez da ondo gorde datu-basean'); history.go(-1);</script>";
+					} else {
+						echo "<script>alert('Erabiltzailea ondo gorde da datu-basean'); window.location.href = '../php/LogInIragarkiak.php'</script>".PHP_EOL;
+					}
+				}
+			}
+            ?>
+		</div>
+	</section>
+	<?php include '../html/Footer.html' ?>
 </body>
 
 </html>
