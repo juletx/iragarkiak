05ef0b0e1d768e81ab79bd9d68313900bc88400d
Aitor Zubillaga <zubillagaaitor@gmail.com>
2019-12-01 19:13:09 +0100

    reformedXubi

--- js/UpdateAccAjax.js
+++ js/UpdateAccAjax.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$("#gorde").click(function () {
 		var form = $("#eguneratu").get(0);
 		$.ajax({
-			url: '../php/GordeAldaketak.php',
+			url: '../php/SaveChanges.php',
 			type: 'POST',
 			data: new FormData(form),
 			mimeType: 'multipart/form-data',
--- php/Menus.php
+++ php/Menus.php
@@ -1,26 +1,24 @@
 <header class='navbar'>
 	<img src="../images/logo.png">
-
+    <button type="button" onclick="location.href='Layout.php'" class="btn btn-primary btn-lg">Hasiera</button>
 <?php if (empty($_SESSION["email"])) { ?>
 
 	<span><a href="LogIn.php" class="buttonIrekiSesioa">Login</a></span>
 	<span><a href="SignUp.php">Erregistratu</a></span>
 
-<?php } else { $email = $_SESSION["email"]; ?>
-
-	<span><a href="ShowAccount.php"><?php echo $email ?></a></span>
+<?php } else { 
+    $email = $_SESSION["email"]; ?>
+    <?php if ($email == "admin@ehu.es") { ?>
+    <span><a href="HandlingAccounts.php">Erabiltzaileak kudeatu</a></span>
+	<span><a href="ManageAccount.php"><?php echo $email ?></a></span>
 	<span><a href="LogOut.php">Logout</a></span>
-	
-<?php if ($email == "admin@ehu.es") { ?>
-
-	<span><a href="HandlingAccounts.php">Erabiltzaileak kudeatu</a></span>
-	
 <?php } else { ?>
-
-	<span><a href="AddAdvertisement.php" class="buttonProduktuaIgo">Iragarkia gehitu</a></span>
-	<span><a href="ShowAdvertisementsUser.php?email=<?php echo $email ?>">Nire iragarkiak</a></span>
-
+    <button type="button" onclick="location.href='AddAdvertisement.php'" class="btn btn-primary btn-lg">Iragarkia gehitu</button>
+    <button type="button" onclick="location.href='ShowAdvertisementsUser.php?email=<?php echo $email ?>'" class="btn btn-primary btn-lg">Nire iragarkiak</button>
+    <button type="button" onclick="location.href='LogOut.php'" class="btn btn-primary btn-lg">Logout</button>
+    <button type="button" onclick="location.href='ManageAccount.php'" class="btn btn-light btn-lg"><?php echo $email ?></button>
+	
 <?php } } ?>
 
-	<span><a href="Layout.php">Hasiera</a></span>
+	
 </header>
