77c53727c89ed7d6d090cb9ed191cd8be8a80051
Julen Etxaniz <juletxara@gmail.com>
2019-12-08 21:21:21 +0100

    Headetik DOCTYPE kendu eta 2 fitxategi formateatu

--- js/GetFullDescription.js
+++ js/GetFullDescription.js
@@ -1,13 +1,11 @@
-
 function GetFullDescription(id) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("home-anuntzio"+id+"-deskripzio").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", '/php/GetAdDescription.php?ad_id='+id, true);
-  xhttp.send();
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("home-anuntzio" + id + "-deskripzio").innerHTML =
+				this.responseText;
+		}
+	};
+	xhttp.open("GET", '/php/GetAdDescription.php?ad_id=' + id, true);
+	xhttp.send();
 }
-
--- php/GetAdDescription.php
+++ php/GetAdDescription.php
@@ -1,20 +1,20 @@
 <?php
-include '../php/DbConfig.php';
-		$link = mysqli_connect($zerbitzaria, $erabiltzailea, $gakoa, $db) or die("Errorea datu-baseko konexioan");
-		$ad_id = trim($_GET["ad_id"]);
-		if (empty($ad_id)) {
-			echo "<script>alert('Id-rik ez dago'); history.go(-1);</script>";
-			exit();
-		}
-		$sql = "SELECT * FROM ads NATURAL JOIN users WHERE ad_id=$ad_id";
-		$result = mysqli_query($link, $sql) or die("Errorea datu-baseko kontsultan");
-		$lerroKopurua = mysqli_num_rows($result);
-		if ($lerroKopurua == 0) {
-			echo "<script>alert('Id okerra'); history.go(-1);</script>";
-		}else{
-			$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-			echo $row['text'];
-		}
+	include '../php/DbConfig.php';
+	$link = mysqli_connect($zerbitzaria, $erabiltzailea, $gakoa, $db) or die("Errorea datu-baseko konexioan");
+	$ad_id = trim($_GET["ad_id"]);
+	if (empty($ad_id)) {
+		echo "<script>alert('Id-rik ez dago'); history.go(-1);</script>";
+		exit();
+	}
+	$sql = "SELECT * FROM ads NATURAL JOIN users WHERE ad_id=$ad_id";
+	$result = mysqli_query($link, $sql) or die("Errorea datu-baseko kontsultan");
+	$lerroKopurua = mysqli_num_rows($result);
+	if ($lerroKopurua == 0) {
+		echo "<script>alert('Id okerra'); history.go(-1);</script>";
+	} else {
+		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		echo $row['text'];
+	}
 ?>
 
 
